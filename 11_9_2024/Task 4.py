7d347b60f370d6ca6f5996c6aea77445d4410ad8
Pallav Kalita <165314390+pallavkalita@users.noreply.github.com>
2024-11-06 11:09:04 +0530

    Update Task 4.py

--- 11_9_2024/Task 4.py
+++ 11_9_2024/Task 4.py
@@ -1,3 +1,13 @@
+"""
+Problem 4: Planets and Distances
+Create a function named list_planets() that returns the following list of planets: "Mercury", "Venus", "Earth", "Mars".
+
+Create a function named distance_from_sun(planet) which receives a string argument representing a planet and returns its average distance from the Sun in millions of kilometers. For example, "Earth" should return "149.6 million km."
+
+Call list_planets() and use distance_from_sun() to print statements about each planet’s distance from the Sun, like "Earth is 149.6 million km away from the Sun."
+
+"""
+
 def list_planets():
     return ["Mercury", "Venus", "Earth", "Mars"]
 
