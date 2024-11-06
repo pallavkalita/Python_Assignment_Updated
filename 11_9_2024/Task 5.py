d851c774aafea5fef81bccbe941eee4b05bdb3cd
Pallav Kalita <165314390+pallavkalita@users.noreply.github.com>
2024-11-06 11:10:03 +0530

    Update Task 5.py

--- 11_9_2024/Task 5.py
+++ 11_9_2024/Task 5.py
@@ -1,3 +1,12 @@
+"""
+Problem 5: Sports and Players
+Create a function named list_sports() that returns the following list of sports: "Football", "Cricket", "Tennis", "Basketball".
+
+Create a function named famous_player(sport) which receives a string argument representing a sport and returns the name of a famous player in that sport. For example, "Football" should return "Lionel Messi".
+
+Call list_sports() and use famous_player() to print statements about each sport and a famous player, like "Lionel Messi is a famous Football player."
+"""
+
 def list_sports():
     return ["Football", "Cricket", "Tennis", "Basketball"]
 
