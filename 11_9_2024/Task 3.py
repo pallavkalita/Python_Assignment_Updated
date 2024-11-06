bd1086ccb9168951c92db82c64d0491fdc24aab3
Pallav Kalita <165314390+pallavkalita@users.noreply.github.com>
2024-11-06 11:08:21 +0530

    Update Task 3.py

--- 11_9_2024/Task 3.py
+++ 11_9_2024/Task 3.py
@@ -1,3 +1,13 @@
+"""
+Problem 3: Animal Sounds
+Create a function named list_animals() that returns the following list of animals: "Dog", "Cat", "Cow", "Lion".
+
+Create a function named animal_sound(animal) which receives a string argument representing an animal and returns the sound it makes. For example, "Dog" should return "barks".
+
+Call list_animals() and use animal_sound() to print statements about each animal and its sound, like "A Dog barks.
+"""
+
+
 def list_animals():
     return ["Dog", "Cat", "Cow", "Lion"]
 
